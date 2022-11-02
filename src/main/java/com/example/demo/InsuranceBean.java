0af89e29febeda0e70cd1bba6599ad2dca805f18
AshwinS12 <ashwin.vtalent@gmail.com>
2022-11-02 14:57:17 +0530

    application props

---
+++ src/main/java/com/example/demo/InsuranceBean.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class InsuranceBean {
+	@Id @GeneratedValue
+	@Column(name="Idno")
+private int Idno;
+	@Column(name="flightCo")
+private String flightCo;
+	@Column(name="traveltype")
+private boolean traveltype;
+	@Column(name="InsurancePremium")
+private int InsurancePremium;
+	
+public int getIdno() {
+	return Idno;
+}
+public void setIdno(int idno) {
+	Idno = idno;
+}
+public String getFlightCo() {
+	return flightCo;
+}
+public void setFlightCo(String flightCo) {
+	this.flightCo = flightCo;
+}
+public boolean isTraveltype() {
+	return traveltype;
+}
+public void setTraveltype(boolean traveltype) {
+	this.traveltype = traveltype;
+}
+public int getInsurancePremium() {
+	return InsurancePremium;
+}
+public void setInsurancePremium(int insurancePremium) {
+	InsurancePremium = insurancePremium;
+}
+@Override
+public String toString() {
+	return "InsuranceBean [Idno=" + Idno + ", flightCo=" + flightCo + ", traveltype=" + traveltype
+			+ ", InsurancePremium=" + InsurancePremium + "]";
+}
+
+
+}
